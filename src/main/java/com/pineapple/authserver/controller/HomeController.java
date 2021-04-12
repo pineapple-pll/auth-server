b39402f98419bc107eecabface9c67c4992e2944
leeyunjea <yunjea0312@naver.com>
2021-04-12 23:43:28 +0900

    update home

--- src/main/java/com/pineapple/authserver/controller/HomeController.java
+++ src/main/java/com/pineapple/authserver/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.pineapple.authserver.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth-server")
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping
     public String home() {
         return "pineapple 인증서버";
     }
